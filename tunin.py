dda3fee2726d85fbf9445f4c3ff5eca83d0c4123
Michela Gravina <mikela.gravina94@gmail.com>
2018-06-26 09:24:57 +0200

    Aggiornamento Tesina e tuning parametri

--- tunin.py
+++ tunin.py
@@ -14,18 +14,38 @@ db = client.YelpDB
 cursor=db.sintesireview.find()
 
 val=np.array([(0.2,0.1,0.7),(0.2,0.2,0.6),(0.2,0.3,0.5),(0.2,0.4,0.4),(0.2,0.5,0.3),(0.2,0.6,0.2),(0.2,0.7,0.1),(0.2,0.8,0),(0.3,0,0.7),(0.3,0.1,0.6),(0.3,0.2,0.5),(0.3,0.3,0.4),(0.3,0.4,0.3),(0.3,0.5,0.2),(0.3,0.6,0.1),(0.3,0.7,0),(0.4,0,0.6),(0.4,0.1,0.5),(0.4,0.2,0.4),(0.4,0.3,0.3),(0.4,0.4,0.2)])
-valori=np.array([])	
-stelle=np.array([])
+
+#val=np.array([(0.2,0.1,0.7),(0.2,0.2,0.6),(0.2,0.3,0.5),(0.2,0.4,0.4)])
+
+valori=[] 
+stelle=[]
 rms=[]
+valorinorm=[]
+
 for t in val:
-	for el in cursor:
-		valori.append(e['valoretext']*(1+t[0]*e['useful']+t[1]*e['cool']+t[2]*e['funny'])*(1+e['vlutente']))
-		stelle.append(0.5*e['stars']-1.5)
-	
-	valorinorm = (2/(max(valori) - min(valori)))*(valori - max(valori)) + 1
-	
-	rms.append(sqrt(mean_squared_error(stelle, valorinorm)))
-		
+    print "stampa la tripla"
+    print t[0],t[1],t[2]
+    for e in cursor:
+        valori.append(e['valoretext']*(1+t[0]*e['useful']+t[1]*e['cool']+t[2]*e['funny'])*(1+e['vlutente']))
+        stelle.append(0.5*e['stars']-1.5)
+    
+    cursor.rewind()
+    #normalizzazione valori
+   # print valori
+    valorinorm = (2/(max(valori)-min(valori)))*(valori - max(valori)) + 1
+   # print valorinorm
+    err=np.sqrt(((valorinorm-stelle)**2).mean())
+    #err=sqrt(mean_squared_error(stelle, valorinorm))
+   # print err
+    f=open("rmsMichela.txt", "a+")
+    f.write("Tupla: %5.1f %5.1f %5.1f , Errore: %5.8f \n" %(t[0],t[1],t[2],err))
+    f.close()
+    rms.append(err)
+    del valori[:]
+    del stelle[:]
+
+                                    
+                                    
 print "Minimo : "
 print min(rms)
 print "Corrispondente a: "
